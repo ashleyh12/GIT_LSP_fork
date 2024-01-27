776431af9ba9809c6351518b90c7664dbed39654
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-01-27 15:24:22 -0500

    test

--- LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
@@ -8,44 +8,63 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Wordcounting {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException 
+    {
         // Grab the file using a relative path
         Path path = Paths.get(System.getProperty("user.dir"))
                 .resolve("words.text");
 
         // Buffer Reads text from a character-input stream, buffering characters so as 
         // to provide for the efficient reading of characters... more efficient than Scanner
-        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) 
+        {
+        	Map<String, Integer> frequency = getWordFrequency(reader);
+        	
+        	// Display the word frequency
+        	for (Map.Entry<String, Integer> entry : frequency.entrySet()) 
+        	{
+        	    System.out.println(entry.getKey() + ": " + entry.getValue());
+        	}
+        	
+        } 
+//       
+//        	catch (IOException e) 
+//        	{
+//        		
+//        	System.err.println("Error reading the file: " + e.getMessage());
+//        	
+//        	}
+        }
         	
         	
+        	
+        	
+        private static Map<String, Integer> getWordFrequency(BufferedReader reader) throws IOException {
+
             // Creating the HashMap
             Map<String, Integer> frequency = new HashMap<>();
 
             // Reading each line from the .txt file
             String line;
-            while ((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) 
+            {
             	
             	
                 // Splitting the words 
                 String[] words = line.split(" ");
 
                 // Counting the frequency of each word found in the file
-                for (String word : words) {
+                for (String word : words) 
+                {
                     word = word.toLowerCase(); // Converting the words to lowercase (ex: Design and design are seen as the same)
                     frequency.put(word, frequency.getOrDefault(word, 0) + 1); //making the word = key, frequency = value being incremented
                 }
             }
+            return frequency;
 
-            // Display the word frequency
-            for (Map.Entry<String, Integer> entry : frequency.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
-        } catch (IOException e) 
-        {
-            System.err.println("Error reading the file: " + e.getMessage());
-        }
     }
 }
 
