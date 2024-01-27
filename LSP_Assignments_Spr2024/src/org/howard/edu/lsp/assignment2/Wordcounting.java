e87d8455f8dcaf426be30810e8f5e5d5e9893398
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-01-27 14:17:03 -0500

    updates

--- LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
@@ -1,9 +1,28 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Wordcounting {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Word Counting!");
+	public static void main(String[] args) throws IOException{
+		//grab the file using a relative path
+		Path path = Paths.get(System.getProperty("user.dir"))
+				.resolve("words.text");
+		//Buffer Reads text from a character-input stream, buffering characters so as 
+//		to provide for the efficient reading of characters... more efficient than Scanner
+		BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+		FileInputStream fin = new FileInputStream("words.text");
+		
+		//creating the HashMap
+		Map<String, Integer> frequency = new HashMap<>();
+	
 
 	}
 
