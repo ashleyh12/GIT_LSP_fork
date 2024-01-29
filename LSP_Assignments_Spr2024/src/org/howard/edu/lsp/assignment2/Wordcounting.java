705ea923e9f949094d25a9ec2dcc51ef7a6da96b
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-01-29 17:17:05 -0500

    test commit

--- LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment2/Wordcounting.java
@@ -16,11 +16,11 @@ public class Wordcounting {
         // Retrieve the words.text file using a relative path
         Path path = Paths.get(System.getProperty("user.dir"))
                 .resolve("words.txt");
-//        
-//        // Get the absolute path of the 
-//        Path absolutePath = path.toAbsolutePath();
-//        System.out.println("Absolute path of the text file is: " + absolutePath);
-//
+        
+        // Get the absolute path of the 
+        Path absolutePath = path.toAbsolutePath();
+        System.out.println("Absolute path of the text file is: " + absolutePath);
+
 
 
   
