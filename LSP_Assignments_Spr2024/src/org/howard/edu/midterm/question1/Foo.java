5cad7e29d669c921f5d95bae9236ddcacce8a0cd
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-03-13 18:59:42 -0400

    test

---
+++ LSP_Assignments_Spr2024/src/org/howard/edu/midterm/question1/Foo.java
@@ -0,0 +1,10 @@
+package org.howard.edu.midterm.question1;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LSP_Assignments_Spr2024/src/org/howard/edu/midterm/question2/Foo.java
@@ -0,0 +1,9 @@
+package org.howard.edu.midterm.question2;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+	}
+
+}
