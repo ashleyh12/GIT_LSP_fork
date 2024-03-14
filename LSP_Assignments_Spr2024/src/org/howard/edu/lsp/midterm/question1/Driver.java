18cc0d457c8fa943c5843c7fbd51b2c3e8357ee9
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-03-14 19:46:37 -0400

    test

--- LSP_Assignments_Spr2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -1,9 +1,10 @@
 package org.howard.edu.lsp.midterm.question1;
 
-public class Foo2 {
+public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
 
 	}
 
