8cbcfdd9450b76f95c7544a33873fd2f0bc29fe0
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-03-14 20:18:21 -0400

    updates to driver.java

--- LSP_Assignments_Spr2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -2,10 +2,13 @@ package org.howard.edu.lsp.midterm.question1;
 
 public class Driver {
 
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello");
+	public static void main(String[] args) {
+		String text = "I love CSCI363";
+		String encryptedText = SecurityOps.encrypt(text);
+		System.out.println("Original text: " + text);
+		System.out.println("Encrypted text: " + encryptedText);
+		}
 
 	}
 
-}
+
