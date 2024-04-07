2935d8157fe96de40d0485d5b3bdd3d238a14a7f
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-04-06 20:48:19 -0400

    test commit to fix workspace issues

---
+++ LSP_Assignments_Spr2024/src/org/howard/edu/lsp/assignment4/IntegerSetException.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment4;
+
+public class IntegerSetException extends Exception {
+
+	public IntegerSetException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
